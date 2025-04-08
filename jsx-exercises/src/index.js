c3989fc6d72dcfe52da4bad5cbf520b8e591cedd
Tseg-tech <tseganehamare@gmail.com>
2025-04-08 12:53:42 +0300

    update

--- jsx-exercises/src/index.js
+++ jsx-exercises/src/index.js
@@ -2,9 +2,72 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 function MyThing(){
-  return(
-    <MyThing/>
-  );
+  /*return(
+   /*<div className='book'>
+    <div className='title'>
+         The Title
+    </div>
+    <div className='author'>
+         The Author
+    </div>
+    <ul className='stats'>
+       <li className='rating'>
+        5 stars
+       </li>
+       <li className='isbn'>
+        12-345678-910
+       </li>
+    </ul>
+   </div>*/
+ /* <div>
+    Empty
+    Newline
+    Test
+     &nbsp;Non-breaking
+    &nbsp;spaces&nbsp;
+    Line 1
+    {' '}
+    Line 2
+   </div>
+  /* <div>
+    &nbsp;Non-breaking
+    &nbsp;spaces&nbsp;
+    {' '}
+    Line 1
+    Line 2 
+   </div>*/   
+  //);
+ return [
+  React.createElement(
+    'div',
+    {className :'title'},
+    'The Title',
+  ),
+  React.createElement(
+    'div',
+    {className:'author'},
+    'The Author'
+  ),
+  React.createElement(
+    'ul',
+    {className:'stats'},
+    React.createElement(
+      'li',
+      {className:'rating'},
+       '5 Stars'
+    ),
+    React.createElement(
+      'li',
+      {className:'isbn'},
+      '12-345678-910'
+    )
+  ),
+  React.createElement(
+    'span',
+    {},
+    'Finalised'
+  ),
+ ]
 }
 
 ReactDOM.createRoot(document.getElementById('root')).render(
